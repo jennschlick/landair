f9b280b03982110204fdc541a632d42a2897eac2
Jenn Schlick <jlschlick@gmail.com>
2020-03-21 09:19:43 -0400

    Remove placeholder form.

--- footer.php
+++ footer.php
@@ -15,13 +15,9 @@
       </div>
     </form>
   <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-  <!-- <form>
-    <input type="email" placeholder="email address">
-    <input type="submit" value="">
-  </form> -->
 </div>
 
-<div class="footer">
+<div class="footer py-6">
   <div class="container">
     <?php if ( is_active_sidebar('footer')) : ?>
       <?php dynamic_sidebar('footer'); ?>
@@ -29,7 +25,7 @@
   </div>
 </div>
 
-<div class="copyright">
+<div class="copyright py-4">
   <div class="container">
     <?php if ( is_active_sidebar('copyright')) : ?>
       <?php dynamic_sidebar('copyright'); ?>
