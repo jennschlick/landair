f73808b0df4d326ebe9e668642dcce74e7f087bb
Jenn Schlick <jlschlick@gmail.com>
2020-03-22 08:09:34 -0400

    Add 404 template.

--- page.php
+++ page.php
@@ -49,10 +49,28 @@
 	<?php endwhile; ?>
 <?php else : ?>
 
-	<div class="container">
-		<h1>404 &mdash; Page not found</h1>
-		<p>This page cannot be found.</p>
-	</div>
+	<article>
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-5 py-5">
+						<h1>404 &mdash; Page not found</h1>
+					</div>
+				</div>
+			</div>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6"></div>
+					<div class="col-md-6 align-self-end">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/header-default-image.jpg" class="img-fluid" alt="" />
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="container py-5">
+			<p>This page cannot be found.</p>
+		</div>
+	</article>
 
 <?php endif; ?>
 
