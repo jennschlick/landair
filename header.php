c74495fc1f119bcb630bff6c60a5aac2b496aef2
Jenn Schlick <jlschlick@gmail.com>
2020-04-02 13:23:30 -0400

    Add site URL.

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 <link rel="apple-touch-icon" sizes="144x144" href="<?php echo site_url(); ?>/apple-icon-144x144.png">
 <link rel="apple-touch-icon" sizes="152x152" href="<?php echo site_url(); ?>/apple-icon-152x152.png">
 <link rel="apple-touch-icon" sizes="180x180" href="<?php echo site_url(); ?>/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="192x192" href="<?php echo site_url(); ?>/android-icon-192x192.png">
 <link rel="icon" type="image/png" sizes="32x32" href="<?php echo site_url(); ?>/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="96x96" href="<?php echo site_url(); ?>/favicon-96x96.png">
 <link rel="icon" type="image/png" sizes="16x16" href="<?php echo site_url(); ?>/favicon-16x16.png">
