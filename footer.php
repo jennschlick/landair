9885262f47ef4aa9d0cbed2a3e49e1d668a3d0ff
Jenn Schlick <jlschlick@gmail.com>
2020-04-05 06:43:34 -0400

    Update prefooter form styles.

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
       		<div class="response" id="mce-success-response" style="display:none"></div>
       	</div>
         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_36eb5bbd529c05b1e7747c5aa_0f5186dd73" tabindex="-1" value=""></div>
-        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button text-hide">
+        <input type="submit" value="" name="subscribe" id="mc-embedded-subscribe" class="button text-hide">
       </div>
     </form>
   <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
