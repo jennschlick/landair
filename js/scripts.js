00d31123052444f990e3412952f7e032b6c0e485
Jenn Schlick <jschlick@mit.edu>
2022-05-08 13:47:58 -0400

    Add function for the tracing page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 (function($){
 
-	$(document).ready(function(){
-		
-	});
+  $(document).ready(function(){
+    $('#protracebutton').click(function(e) {  
+        var inputvalue = $("#protraceinput").val();
+        window.location.replace("https://tracking.carrierlogistics.com/scripts/laxv.pol/facts.htm?startpage=protrace&pronum="+inputvalue);
+    });
+  });
 
 })(jQuery)
